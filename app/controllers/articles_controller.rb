f6a206b5325ff3e17a6650f4338468a42669f508
Lina <117147821+LinA636@users.noreply.github.com>
2023-11-24 12:25:37 +1300

    create a view that shows the first article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,9 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  # show will by default render app/views/articles/show.html.erb
+  def show
+    @article = Article.find(params[:id])
+  end
 end
