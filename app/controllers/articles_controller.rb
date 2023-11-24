eb71af97486194cfdfd25b99f19fa4a6a15f0b5a
Lina <117147821+LinA636@users.noreply.github.com>
2023-11-24 13:11:40 +1300

    create new view for new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,24 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  # instantiates a new article, but does not save it.
+  # Will be used in the view whenbuilding the form.
+  # by default renders app/views/articles/new.html.erb
+  def new
+    @article = Article.new
+  end
+
+  # instantiates a new article and attempts to save it.
+  # If the save is successful, the action redirects the browser to the article's page.
+  # Else the action redisplays the form with an error message.
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
